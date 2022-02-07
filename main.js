4a67b0b96d2680e2f46e6f50cab7a98f28ffe936
MatteoFar <matteo.farigu@gmail.com>
2022-02-07 15:01:48 +0100

    exo3 done

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const helloWorld = <h1>Hello world!</h1>;
+
+const firstname = 'matteo'
+const lastname = 'farigu'
+
+const toUpperCase = (name, name1) => {
+  let a = name.charAt(0).toUpperCase() + name.slice(1)
+  let b = name1.charAt(0).toUpperCase() + name1.slice(1)
+  return a + ' ' + b;
+}
+
+const helloWorld = <div className="test">{toUpperCase(firstname, lastname)}</div>;
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
