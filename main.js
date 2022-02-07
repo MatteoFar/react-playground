1ebe328d0d4a8450a6c0964baeffc3c7c6dbb4f5
MatteoFar <matteo.farigu@gmail.com>
2022-02-07 15:22:33 +0100

    exo 4 done

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ const lastName = 'formateur';
 
 
 // Solution sans bonus
-const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
+
+
+
+function FirstName(props) {
+
+  function toUpperCase(name) {
+    return name[0].toUpperCase() + name.slice(1)
+  }
+
+  return <span>{toUpperCase(props.firstName)}</span>;
+}
+
+function LastName(props) {
+
+  function toUpperCaseColor(lastname) {
+    return lastName.toUpperCase()
+  }
+
+  return <span className="red-text">{toUpperCaseColor(props.lastName)}</span>
+}
+
+const helloWorld = <div>
+  <FirstName firstName={firstName} />
+  <LastName lastName={lastName} />
+  </div>
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
